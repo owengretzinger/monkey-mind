f7099431ccebfbcbef98c84fa7c73ab8dd4c3739
Owen Gretzinger <owengretzinger@gmail.com>
2025-01-18 02:36:34 -0500

    basic popup ui

--- chrome-extension-boilerplate-react-vite/pages/popup/src/Popup.tsx
+++ chrome-extension-boilerplate-react-vite/pages/popup/src/Popup.tsx
@@ -1,78 +1,32 @@
 import '@src/Popup.css';
 import { useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
-import { exampleThemeStorage } from '@extension/storage';
-import type { ComponentPropsWithoutRef } from 'react';
-
-const notificationOptions = {
-  type: 'basic',
-  iconUrl: chrome.runtime.getURL('icon-34.png'),
-  title: 'Injecting content script error',
-  message: 'You cannot inject script here!',
-} as const;
+import { HATS, hatStorage } from '@extension/storage';
 
 const Popup = () => {
-  const theme = useStorage(exampleThemeStorage);
-  const isLight = theme === 'light';
-  const logo = isLight ? 'popup/logo_vertical.svg' : 'popup/logo_vertical_dark.svg';
-  const goGithubSite = () =>
-    chrome.tabs.create({ url: 'https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite' });
-
-  const injectContentScript = async () => {
-    const [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
-
-    if (tab.url!.startsWith('about:') || tab.url!.startsWith('chrome:')) {
-      chrome.notifications.create('inject-error', notificationOptions);
-    }
-
-    await chrome.scripting
-      .executeScript({
-        target: { tabId: tab.id! },
-        files: ['/content-runtime/index.iife.js'],
-      })
-      .catch(err => {
-        // Handling errors related to other paths
-        if (err.message.includes('Cannot access a chrome:// URL')) {
-          chrome.notifications.create('inject-error', notificationOptions);
-        }
-      });
-  };
+  const selectedHat = useStorage(hatStorage);
 
   return (
-    <div className={`App ${isLight ? 'bg-slate-50' : 'bg-gray-800'}`}>
-      <header className={`App-header ${isLight ? 'text-gray-900' : 'text-gray-100'}`}>
-        <button onClick={goGithubSite}>
-          <img src={chrome.runtime.getURL(logo)} className="App-logo" alt="logo" />
-        </button>
-        <p>
-          Edit <code>pages/popup/src/Popup.tsx</code>
-        </p>
-        <button
-          className={
-            'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
-            (isLight ? 'bg-blue-200 text-black' : 'bg-gray-700 text-white')
-          }
-          onClick={injectContentScript}>
-          Click to inject Content Script
-        </button>
-        <ToggleButton>Toggle theme</ToggleButton>
+    <div className={`App bg-slate-50`}>
+      <header className={`App-header text-gray-900`}>
+        <div className="flex flex-col items-center justify-center space-y-4 p-4">
+          <h1 className="">Monkey Mind</h1>
+          <div className="pt-4">
+            <img src={chrome.runtime.getURL('monkey.png')} alt="logo" className="size-16" />
+          </div>
+          <div className="">
+            <p className="mb-2">Choose your hat</p>
+            <div className="flex flex-row flex-wrap justify-center gap-4">
+              {HATS.map(hat => (
+                <button
+                  key={hat}
+                  className={`size-8 rounded-xl bg-amber-900/15 ${selectedHat === hat && 'ring-2 ring-amber-800'}`}
+                  onClick={() => hatStorage.setHat(hat)}></button>
+              ))}
+            </div>
+          </div>
+        </div>
       </header>
     </div>
-  );
-};
-
-const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
-  const theme = useStorage(exampleThemeStorage);
-  return (
-    <button
-      className={
-        props.className +
-        ' ' +
-        'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
-        (theme === 'light' ? 'bg-white text-black shadow-black' : 'bg-black text-white')
-      }
-      onClick={exampleThemeStorage.toggle}>
-      {props.children}
-    </button>
   );
 };
 
